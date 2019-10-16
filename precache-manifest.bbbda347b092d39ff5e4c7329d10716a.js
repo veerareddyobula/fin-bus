6da584b665fe0bf2b6dd41a4350c4e4589134193
veera <v-vobulareddy@expedia.com>
2019-10-16 12:49:02 +0530

    Updates

---
+++ precache-manifest.bbbda347b092d39ff5e4c7329d10716a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618d4ea8089c01b3ff75825d338b3d41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdce52623250450e701e",
+    "url": "/static/css/main.b6f51580.chunk.css"
+  },
+  {
+    "revision": "e879877472438b8f6e7d",
+    "url": "/static/js/2.3029b86e.chunk.js"
+  },
+  {
+    "revision": "bdce52623250450e701e",
+    "url": "/static/js/main.f019b1eb.chunk.js"
+  },
+  {
+    "revision": "3141f9eab5f880b51f29",
+    "url": "/static/js/runtime-main.b9740a44.js"
+  }
+]);
